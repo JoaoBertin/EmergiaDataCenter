33e2b17fc9c452facfa2a7a23eebbfca5e58d520
DaviCCTI <davicancian26ti@gmail.com>
2025-04-25 11:44:57 -0300

    Inicio tela de Consulta

--- src/Model/Consulta.java
+++ src/Model/Consulta.java
@@ -13,5 +13,44 @@ public class Consulta {
     private Cliente cliente;
     private DataCenter datacenter;
     private float consumoMensalKWh;
+
+    public Consulta(int id, Cliente cliente, DataCenter datacenter, float consumoMensalKWh) {
+        this.id = id;
+        this.cliente = cliente;
+        this.datacenter = datacenter;
+        this.consumoMensalKWh = consumoMensalKWh;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public DataCenter getDatacenter() {
+        return datacenter;
+    }
+
+    public void setDatacenter(DataCenter datacenter) {
+        this.datacenter = datacenter;
+    }
+
+    public float getConsumoMensalKWh() {
+        return consumoMensalKWh;
+    }
+
+    public void setConsumoMensalKWh(float consumoMensalKWh) {
+        this.consumoMensalKWh = consumoMensalKWh;
+    }
     
 }
